0a391afeec3ab8da85689fafe7bbf2ab14e1c719
yoshiyuki-tsuchida <Yoshiyuki_Tsuchida@voyagegroup.com>
2013-06-12 06:28:11 +0900

    重ならない乱数座標を生成

--- game.js
+++ game.js
@@ -13,8 +13,6 @@ window.onload = function() {
     var game = new Game(320, 320);
     game.fps = 16;
     game.score = 0;
-    var label;
-    var bear;
 
     //画像読み込み
     game.preload('http://enchantjs.com/assets/images/chara1.gif',
@@ -38,17 +36,67 @@ window.onload = function() {
     //これを呼び出すと、個数分の円の登録が完了する
     game.addCircle = function() {
         //サーフェイスオブジェクトの生成
+        var surface = new Surface(50,50);
+
         //サーフェイスの色を指定
+        surface.setColor("rgb(0,0,0)",CIRCLE_COLOR);
+
+        //座標記憶用配列の準備
+        var tmp = Array(CIRCLE_NUM);
+        for (var i = 0; i < CIRCLE_NUM; i++) {
+            tmp[i] = Array(2);
+        }
+
         //for文の開始（円の個数分）
-        //ランダムな座標を生成
-        //ランダムな座標は配列に記憶しておき、重ならないように処理する
-        //ランダムな座標が決定
-        //Sprite[i]の生成
-        //imageにsurfaceを指定
-        //ランダム座標を元に、sprite[i]のx,yを決定する
-        //ルートシーンにsprite[i]を追加する
-        //タッチされたときのsprite[i]の処理を書く（エフェクト）
-        //for文終わり
+        for (var i = 0; i < CIRCLE_NUM; i++) {
+
+            var isFull = false;
+            var s = 0;
+
+            while (!isFull) {
+                //ランダムな座標を生成
+                x = rand(280) + 10;
+                y = rand(280) + 10;
+
+                var isDuplication = false;
+                for (var j = 0; j < tmp.length; j++) {
+                    if (tmp[j][0] - 10 < x && x < tmp[j][0] + 10) {
+                        isDuplication = true;
+                    }
+                    if (tmp[j][1] - 10 < y && y < tmp[j][1] + 10) {
+                        isDuplication = true;
+                    }
+                }
+
+                if (isDuplication) {
+                    continue;
+                }
+
+                tmp[i][0] = x;
+                tmp[i][1] = y;
+
+                s++;
+
+                if (s >= 10) {
+                    isFull = true;
+                }
+            }
+            console.log("END");
+
+            for (var j = 0; j < tmp.length; j++) {
+                console.log(tmp[j][0]);
+            }
+            //ランダムな座標は配列に記憶、重ならないように処理する
+            //for (var j = 0; j < tmp.length; j++) {
+            //    if (tmp[j])
+            //ランダムな座標が決定
+            //Sprite[i]の生成
+            //imageにsurfaceを指定
+            //ランダム座標を元に、sprite[i]のx,yを決定する
+            //ルートシーンにsprite[i]を追加する
+            //タッチされたときのsprite[i]の処理を書く（エフェクト）
+            //for文終わり
+        }
         //これで個数分のspriteの登録が終わった
     };
 
@@ -59,7 +107,7 @@ window.onload = function() {
         //背景スプライトの生成
         var bg = new Sprite(320, 320);
         //背景色の指定
-        bg.backgroundColor = "rgb(0, 200, 250)";
+        bg.backgroundColor = "rgb(100, 100, 100)";
         //ルートシーンに追加
         game.rootScene.addChild(bg);
 
